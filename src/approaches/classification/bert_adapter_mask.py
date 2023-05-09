52238dfc01a99e31f166567e7a84dab0e9097077
dionis <inoid2007@gmail.com>
2023-05-09 05:31:08 -0400

    Execute remote and fix error.

--- src/approaches/classification/bert_adapter_mask.py
+++ src/approaches/classification/bert_adapter_mask.py
@@ -219,8 +219,9 @@ class Appr(ApprBase):
 
         target_list = []
         pred_list = []
-        badClassifierRecord = {0:0,1:0,2:0}
-        labelTag = {0: "negative", 1: "neutral", 2: "positive"}
+        badClassifierRecord = {0:0,1:0,2:0,3:0,4:0,5:0}
+        #labelTag = {0: "negative", 1: "neutral", 2: "positive"}
+        labelTag = {1: "very_bad", 2: "bad", 3: "neutral",4:"good",5:"very_good"}
 
         with torch.no_grad():
             badClassfiSentenceAll = []
